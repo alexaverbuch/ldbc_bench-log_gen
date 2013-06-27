bc7c6cbca620e3637904703fca952e01505a20aa
Alex Averbuch <alex.averbuch@gmail.com>
2013-06-27 05:52:43 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ IN_PROJECT_MVN_REPO="lib"
 git submodule update
 rm -rf $IN_PROJECT_MVN_REPO
 cd $LDBC_CORE
+git pull
 ./build.sh
 cd ..
 mvn install:install-file -DlocalRepositoryPath=$IN_PROJECT_MVN_REPO -DcreateChecksum=true -Dpackaging=jar -Dfile=$LDBC_CORE_JAR -DgroupId=com.ldbc.driver -DartifactId=core -Dversion=$LDBC_CORE_VER
